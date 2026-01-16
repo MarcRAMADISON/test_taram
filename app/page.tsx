2fdb1092ffc8e57291925dfabaa6fefeb651993e
Your Name <you@example.com>
2026-01-16 19:49:17 +0100

    finish project test

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,54 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-col justify-center items-center py-15 px-1">
+      <div className="flex flex-row items-center justify-center bg-white max-w-[90%]  shadow-[0px_0px_20px_#0ABC8C] rounded-full py-3 px-6 mb-15">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+        className='mr-2 sm:w-2 sm:h-2 md:w-5 md:h-5 xl:w-10 xl:h-10'
+          src="/assert/logo_Bulle.png"
+          alt="search logo"
+          width={40}
+          height={40}
+        ></Image>
+        <h1 className="text-black text-sm md:text-xl xl:text-2xl font-bold uppercase ">
+          CRÉER UN SITE WEB <span className="underline">VRAIMENT</span> UNIQUE
+        </h1>
+      </div>
+      <h1 className="text-white font-bold text-3xl md:text-4xl xl:text-5xl max-w-180 text-center leading-normal">
+        Votre <span className="text-[#0ABC8C]">site</span> doit{" "}
+        <span className="text-[#0ABC8C]">donner envie </span>de rester, pas de{" "}
+        <span className="line-through">
+        <span className="text-[#0ABC8C]">revenir</span> en{" "}
+        <span className="text-[#0ABC8C]">arrière.</span>
+        </span>
+      </h1>
+      <h2 className="text-md text-white max-w-150 flex-wrap my-5 text-center">
+        Design moderne, SEO solide, Suivi complet : on construit un site qui
+        retient vos visiteurs et vous apporte des résultats.
+      </h2>
+      <div className="flex flex-row justify-center flex-wrap max-w-600 mt-10 gap-y-20 items-center">
+        <div className="flex relative flex-row items-center justify-center bg-[#0ABC8C] py-3 px-5 rounded-lg">
+          <Image
+          className="mr-5"
+            src="/assert/calendar.png"
+            alt="calendrier"
+            width={30}
+            height={30}
+            priority
+          ></Image>
+          <Image className="absolute bottom-[-60px] left-[-40px] " src='/assert/un_Ptit_click.png' alt='ptit click' width={150} height={60}></Image>
+          <p className='text-black text-lg font-bold'>Prendre RDV</p>
+          <div className="absolute top-0 left-0 bg-gradient-to-r from-transparent to-black w-full h-full opacity-50"></div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex flex-row items-center ml-5 justify-center bg-none border-[#0ABC8C] border-1 rounded-lg py-3 px-5 ">
+          <p className="text-lg font-bold text-[#0ABC8C] ">Découvrir nos projets</p>
+          <Image className='ml-4' src="/assert/down_arrow.png" alt="down arrow" width={20} height={20}>
+          </Image>
         </div>
-      </main>
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 }
